4f22fc491d6c4f6a5b82b77e4f1146ec191360d2
GuilhermeGarap <guilhermegarap@gmail.com>
2026-01-20 15:36:42 -0300

    Inicio da Configuração de Testes

---
+++ src/test/java/com/aluguelcarros_vrs1/ApiApplicationTestes.java
@@ -0,0 +1,14 @@
+package com.aluguelcarros_vrs1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTestes {
+
+
+    @Test
+    void main() {
+        AluguelcarrosVrs1Application.main(new String[] {});
+    }
+}
