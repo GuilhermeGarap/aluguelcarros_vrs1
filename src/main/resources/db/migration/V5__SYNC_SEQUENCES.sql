9321d18f2032311486d21b72532a5a37cb24d769
GuilhermeGarap <guilhermegarap@gmail.com>
2026-02-18 11:04:55 -0300

    Sincronização das sequências de IDs com os valores máximos das tabelas no script de migração

---
+++ src/main/resources/db/migration/V5__SYNC_SEQUENCES.sql
@@ -0,0 +1,23 @@
+SELECT setval(
+               pg_get_serial_sequence('usuarios', 'id'),
+               COALESCE((SELECT MAX(id) FROM usuarios), 1),
+               true
+       );
+
+SELECT setval(
+               pg_get_serial_sequence('clientes', 'id'),
+               COALESCE((SELECT MAX(id) FROM clientes), 1),
+               true
+       );
+
+SELECT setval(
+               pg_get_serial_sequence('carros', 'id'),
+               COALESCE((SELECT MAX(id) FROM carros), 1),
+               true
+       );
+
+SELECT setval(
+               pg_get_serial_sequence('alugueis', 'id'),
+               COALESCE((SELECT MAX(id) FROM alugueis), 1),
+               true
+       );
